ec45d3234453b546cf9653a7ff4522b810eeb3eb
Chicago Duan <duanzhijia01@inspur.com>
2020-12-21 17:34:00 +0800

    Add files via upload

---
+++ openbmc_mail_search.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#Author: ChicagoDuan(duanzhijia01@inspur.com)
+
+searchContent=$1
+
+currentYear=`date +%Y`
+currentMonth=`date +%B`
+
+#declare -a arrayMonth
+arrayMonth=("January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December")
+
+mkdir openbmc_mail_log 2>/dev/null
+cd openbmc_mail_log
+
+echo "Start downloading all mails, please wait patiently"
+
+for((i=2015;i<=currentYear;i++))
+do
+	for j in {0..11};do
+	
+		if [[ $i == 2015 && $j -lt 8 ]]
+		then
+			continue
+		fi
+		
+		if [ -f "$i-${arrayMonth[$j]}.txt" ]; then
+			continue
+		fi
+
+		wget https://lists.ozlabs.org/pipermail/openbmc/$i-${arrayMonth[$j]}.txt.gz 2>> log.txt
+		gzip -d $i-${arrayMonth[$j]}.txt.gz 2>> log.txt
+			
+		if [ "$i-${arrayMonth[$j]}.txt" == ""$currentYear-$currentMonth.txt"" ]; then
+			break	
+		fi
+	done
+
+done
+
+grep -riwn "$searchContent" *
+
+exit
